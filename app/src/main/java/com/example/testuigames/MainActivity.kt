4dcce84a2f205524b1143d6d985dd803b8ed76a7
Dmitry <d.diuan455@gmail.com>
2024-03-11 17:15:12 +0300

    process

--- app/src/main/java/com/example/testuigames/MainActivity.kt
+++ app/src/main/java/com/example/testuigames/MainActivity.kt
@@ -3,30 +3,46 @@ package com.example.testuigames
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.testuigames.ui.theme.TestUIGamesTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -38,65 +54,80 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GameScreen(){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(horizontal = 20.dp),
-        verticalArrangement = Arrangement.Top,
+    Column(modifier = Modifier.padding(vertical = 15.dp, horizontal = 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally) {
-        TitleText(text = "Угадай число", textSize = 20.sp)
-        TitleText(text ="Я думаю о числе от 1 до 10. Сможешь угадать?" ,
-            textSize = 26.sp )
-        InputField()
-        CheckButton("Ввод", 20.sp)
+        Text(fontSize = 16.sp,
+            style = TextStyle(color = Color.Black),
+            text = stringResource(R.string.title))
+        TitleText(stringResource = R.string.condition)
+        ProcessingUserInput()
 
     }
+
 
 }
 
 @Composable
-fun TitleText(modifier: Modifier = Modifier, text: String, textSize: TextUnit){
-    Row(modifier = Modifier.padding(vertical = 30.dp),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(text = text,
-            modifier = modifier,
-            style = TextStyle(fontSize = textSize)
-        )
-    }
-}
-
-@Composable
-fun CheckButton(buttonText: String, buttonTextSize: TextUnit){
-    Row(modifier = Modifier
+fun TitleText(stringResource: Int){
+    Text(modifier = Modifier
         .fillMaxWidth()
-        .padding(10.dp),
-        horizontalArrangement = Arrangement.Center){
-        Button(onClick = { /*TODO*/ },
-            modifier = Modifier.size(width = 400.dp, height = 40.dp)) {
-            Text(text = buttonText,
-                fontSize = buttonTextSize)
+        .padding(vertical = 25.dp),
+        style = TextStyle(color = Color.Black),
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center,
+        text = stringResource(stringResource))
+}
+
+@Composable
+fun ProcessingUserInput() {
+    val userInput = remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        TextField(value = userInput.value , onValueChange = {newInput ->
+            userInput.value = newInput
+        }, modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp) )
+        Button(onClick = {
+
+
+        },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 15.dp)
+                .height(50.dp)
+                ) {
+            Text(text = stringResource(R.string.input),
+                style = TextStyle(color = Color.White,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold),
+
+
+
+            )
+
         }
+
     }
-}
-
-@Composable
-fun InputField(){
 
 
-    TextField(value = "", onValueChange = {} )
 }
 
 @Preview
 @Composable
-fun Preview(){
+fun PreviewProcessingUserInput(){
     TestUIGamesTheme {
-        CheckButton("Ввод", 20.sp)
+        ProcessingUserInput()
     }
 }
 
 @Preview
 @Composable
-fun PreviewGameScreen(){
+fun PreviewGameScree(){
     TestUIGamesTheme {
         GameScreen()
     }
---
+++ app/src/main/java/com/example/testuigames/ViewModel/GameViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.testuigames.ViewModel
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.testuigames.R
+
+sealed class GameState {
+    data class InputRequest(@StringRes val titleResource: Int) : GameState()
+    data class Win(@StringRes val titleResource: Int) : GameState()
+    data class Game(@StringRes val hintResource: Int, val attempts: Int) : GameState()
+
+}
+
+
+class GameViewModel : ViewModel() {
+
+    private var random = randomNumbers()
+    private var attempts = 0
+
+    private val _gameState = MutableLiveData<GameState>()
+    val gameState: LiveData<GameState>
+        get() = _gameState
+
+    init {
+        startNewGame()
+    }
+
+    private fun startNewGame() {
+        random = randomNumbers()
+        attempts = 0
+        _gameState.value = GameState.InputRequest(R.string.input_request)
+    }
+
+    fun checkUserInput(userInput: Int) {
+        attempts++
+        if (userInput == random) {
+            _gameState.value = GameState.Win(R.string.request)
+
+        } else if (userInput > random) {
+            _gameState.value = GameState.Game(R.string.hint1, attempts)
+
+        } else {
+            _gameState.value = GameState.Game(R.string.hint2, attempts)
+        }
+
+
+    }
+
+    fun restartTheGame() {
+        startNewGame()
+    }
+
+    private fun randomNumbers(): Int {
+        val min = 1
+        val max = 10
+        return (min..max).random()
+    }
+
+
+}
