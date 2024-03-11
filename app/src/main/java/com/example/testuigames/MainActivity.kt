ff6254f178c76b62cebe46246f84b404fc03bc31
Dmitry <d.diuan455@gmail.com>
2024-03-11 19:43:31 +0300

    process2

--- app/src/main/java/com/example/testuigames/MainActivity.kt
+++ app/src/main/java/com/example/testuigames/MainActivity.kt
@@ -3,25 +3,20 @@ package com.example.testuigames
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,38 +24,77 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.testuigames.ViewModel.GameState
+import com.example.testuigames.ViewModel.GameViewModel
 import com.example.testuigames.ui.theme.TestUIGamesTheme
-import org.w3c.dom.Text
+
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GameScreen()
+            GameScreenState()
 
         }
     }
 }
 
+
 @Composable
-fun GameScreen(){
-    Column(modifier = Modifier.padding(vertical = 15.dp, horizontal = 10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(fontSize = 16.sp,
+fun GameScreenState(viewModel: GameViewModel = viewModel()) {
+    val gameState by viewModel.gameState.observeAsState()
+    when (gameState) {
+        is GameState.InputRequest -> {
+            GameScreen()
+        }
+
+        is GameState.Win -> {
+            GameScreenRestart(
+                activity = MainActivity(),
+                stringResource = (gameState as GameState.Win).titleResource
+            )
+        }
+
+        is GameState.Game -> {
+            Column {
+                GameScreen()
+                HiltText(hintResource = (gameState as GameState.Game).hintResource)
+
+            }
+        }
+
+
+        else -> {}
+    }
+
+}
+
+@Composable
+fun GameScreen() {
+
+
+    Column(
+        modifier = Modifier.padding(vertical = 15.dp, horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            fontSize = 16.sp,
             style = TextStyle(color = Color.Black),
-            text = stringResource(R.string.title))
-        TitleText(stringResource = R.string.condition)
+            text = stringResource(R.string.title)
+        )
+        TitleText(stringResource = R.string.input_request)
         ProcessingUserInput()
+
+
 
     }
 
@@ -68,47 +102,83 @@ fun GameScreen(){
 }
 
 @Composable
-fun TitleText(stringResource: Int){
-    Text(modifier = Modifier
+fun HiltText(hintResource: Int,){
+    Row(modifier = Modifier
         .fillMaxWidth()
-        .padding(vertical = 25.dp),
-        style = TextStyle(color = Color.Black),
-        fontSize = 24.sp,
-        textAlign = TextAlign.Center,
-        text = stringResource(stringResource))
+        .padding(horizontal = 10.dp),
+        horizontalArrangement = Arrangement.SpaceAround) {
+        Text(
+            fontSize = 20.sp,
+            style = TextStyle(color = Color.Black),
+            text = stringResource(R.string.hint),
+            fontWeight = FontWeight.Bold
+        )
+        Text(stringResource(hintResource),
+            fontSize = 18.sp,
+            style = TextStyle(color = Color.Black)
+            )
+
+
+    }
+
 }
 
 @Composable
-fun ProcessingUserInput() {
+fun TitleText(stringResource: Int) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 25.dp),
+        style = TextStyle(color = Color.Black),
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center,
+        text = stringResource(stringResource)
+    )
+}
+
+@Composable
+fun ProcessingUserInput(viewModel: GameViewModel = viewModel()) {
+
     val userInput = remember {
         mutableStateOf("")
     }
 
-    Column(modifier = Modifier,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        TextField(value = userInput.value , onValueChange = {newInput ->
-            userInput.value = newInput
-        }, modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp) )
-        Button(onClick = {
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = userInput.value, onValueChange = { newInput ->
+                userInput.value = newInput
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+        )
+        Button(
+            onClick = {
+                viewModel.checkUserInput(userInput.value.toInt())
+                userInput.value = ""
 
 
-        },
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 15.dp)
                 .height(50.dp)
-                ) {
-            Text(text = stringResource(R.string.input),
-                style = TextStyle(color = Color.White,
+        ) {
+            Text(
+                text = stringResource(R.string.input),
+                style = TextStyle(
+                    color = Color.White,
                     fontSize = 20.sp,
                     textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold),
+                    fontWeight = FontWeight.Bold
+                ),
 
 
-
-            )
+                )
 
         }
 
@@ -117,18 +187,71 @@ fun ProcessingUserInput() {
 
 }
 
-@Preview
 @Composable
-fun PreviewProcessingUserInput(){
-    TestUIGamesTheme {
-        ProcessingUserInput()
+fun GameScreenRestart(
+    viewModel: GameViewModel = viewModel(),
+    activity: MainActivity,
+    stringResource: Int
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 100.dp, horizontal = 20.dp)
+    ) {
+        TitleText(stringResource = stringResource)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 40.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = { viewModel.restartTheGame() },
+                modifier = Modifier.size(width = 100.dp, height = 50.dp)
+            ) {
+                Text(
+                    stringResource(R.string.yes),
+                    fontSize = 18.sp,
+                    style = TextStyle(color = Color.White),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Button(
+                onClick = { activity.finish() },
+                modifier = Modifier.size(width = 100.dp, height = 50.dp)
+            ) {
+                Text(
+                    stringResource(R.string.no),
+                    fontSize = 18.sp,
+                    style = TextStyle(color = Color.White),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+
+            }
+        }
     }
 }
 
 @Preview
 @Composable
-fun PreviewGameScree(){
+fun PreviewProcessingUserInput() {
     TestUIGamesTheme {
-        GameScreen()
+        Column {
+            GameScreen()
+            HiltText(hintResource = (R.string.hint1))
+
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewGameRestart() {
+    TestUIGamesTheme {
+        //GameScreenRestart(activity = ComponentActivity())
     }
 }
